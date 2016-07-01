c879e646109f6c404f930dfc9df9b23027498ecb
Ivan Demidov <demidov@220-volt.ru>
2016-07-01 15:57:55 +0300

    Fixed #1, init project

--- README.md
+++ README.md
@@ -1,1 +1,122 @@
-# posthtml-beautify
+# <a href="https://github.com/posthtml/posthtml"><img valign="text-bottom" height="49" title="PostHTML logo" src="http://posthtml.github.io/posthtml/logo.svg"></a> beautify
+
+> A [posthtml](https://github.com/posthtml) plugin to beautify you html files
+
+[![Travis Build Status](https://img.shields.io/travis/GitScrum/posthtml-beautify.svg?style=flat-square&label=unix)](https://travis-ci.org/GitScrum/posthtml-beautify)[![AppVeyor Build Status](https://img.shields.io/appveyor/ci/GitScrum/posthtml-beautify.svg?style=flat-square&label=windows)](https://ci.appveyor.com/project/GitScrum/posthtml-beautify)[![npm version](https://img.shields.io/npm/v/posthtml-beautify.svg?style=flat-square)](https://www.npmjs.com/package/posthtml-beautify)[![Dependency Status](https://david-dm.org/gitscrum/posthtml-beautify.svg?style=flat-square)](https://david-dm.org/gitscrum/posthtml-beautify)[![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg?style=flat-square)](https://github.com/sindresorhus/xo)[![Coveralls status](https://img.shields.io/coveralls/GitScrum/posthtml-beautify.svg?style=flat-square)](https://coveralls.io/r/GitScrum/posthtml-beautify)
+
+[![npm downloads](https://img.shields.io/npm/dm/posthtml-beautify.svg?style=flat-square)](https://www.npmjs.com/package/posthtml-beautify)[![npm](https://img.shields.io/npm/dt/posthtml-beautify.svg?style=flat-square)](https://www.npmjs.com/package/posthtml-beautify)[![Package Quality](http://npm.packagequality.com/shield/posthtml-beautify.svg?style=flat-square)](http://packagequality.com/#?package=posthtml-beautify)
+
+## Why?
+Format your html and inline css markup according to the [HTML5 syntax Style Guide](http://www.w3schools.com/html/html5_syntax.asp), [Code Guide](http://codeguide.co/#html). Full list of supported options:
+- [ ] Transform lower case element names
+- [ ] Transform lower case attribute names
+- [ ] Only double quotes
+- [x] Close all html elements 
+- [ ] Removing trailing slash in self-closing 
+- [ ] Removes spaces at the equal sign
+- [x] Add blank lines to separate large or logical code blocks
+- [x] Add 2 spaces of indentation. *Do not use TAB*.
+- [ ] Add language attribute
+- [ ] Add character encoding
+- [ ] Attribute order
+- [ ] Boolean attributes :interrobang:
+- [ ] Creates file from the inline styles
+- [ ] Create scoped class name (*use css-modules*) instead inline styles
+
+## Install
+
+```bash
+npm i -S posthtml posthtml-beautify
+```
+
+> **Note:** This project is compatible with node v4+
+
+## Usage
+
+```js
+import {readFileSync, writeFileSync} from 'fs';
+import posthtml from 'posthtml';
+import beautify from 'posthtml-beautify';
+
+const html = fs.readFileSync('input.html', 'utf8');
+
+posthtml()
+    .use(beautify(/* options */))
+    .process(html)
+    .then(result => {
+        fs.writeFileSync('output.html', result.html);
+    });
+
+```
+
+## Example
+
+#### input.html
+```html
+<!DOCTYPE html>
+
+ <table>
+  <tr>
+    <th>Name</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>A</td>
+    <td>Description of A</td>
+  </tr>
+  <tr>
+    <td>B</td>
+    <td>Description of B</td>
+  </tr>
+</table>
+    <div>
+<div class="first">
+                first</div>   <div class="middle"></div>
+<div class="last">last <b>line  </b> <a href="#">            test</a></div>
+
+</div>
+<img src="img.png" alt=""><input type="text">
+```
+
+#### output.html
+```html
+<!DOCTYPE html>
+<table>
+  <tr>
+    <th>Name</th>
+
+    <th>Description</th>
+  </tr>
+
+  <tr>
+    <td>A</td>
+
+    <td>Description of A</td>
+  </tr>
+
+  <tr>
+    <td>B</td>
+
+    <td>Description of B</td>
+  </tr>
+</table>
+<div>
+  <div class="first">first</div>
+</div>
+<div class="middle"></div>
+<div class="last">
+  last
+
+  <b>line</b>
+
+  <a href="#">test</a>
+</div>
+<img src="img.png" alt="">
+<input type="text">
+```
+
+## Options
+
+#### `indent`
+Type: `Object`  
+Default: `{ type: 'space', size: 2 }`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+const optionsDefault = {
+	indent: {
+		type: 'space',
+		size: 2
+	}
+};
+
 const clean = tree => tree
 	.filter(node => {
-		return typeof node === 'object' || (typeof node === 'string' && (node.trim().length !== 0 || /doctype/gi.test(node)))
+		return typeof node === 'object' || (typeof node === 'string' && (node.trim().length !== 0 || /doctype/gi.test(node)));
 	})
 	.map(node => {
 		if (Object.prototype.hasOwnProperty.call(node, 'content')) {
@@ -9,308 +16,57 @@ const clean = tree => tree
 		return typeof node === 'string' ? node.trim() : node;
 	});
 
-const getIndent = level => `\n${new Array(level).join('    ')}`;
+const indent = (tree, {indent: {type, size}}) => {
+	const indentString = type === 'space' ? ' '.repeat(size) : '\t';
 
-const indent = (tree, level = 0) => tree.reduce((previousValue, currentValue, index) => {
+	const getIndent = level => `\n${indentString.repeat(level)}`;
 
-	if (typeof currentValue === 'object' && Object.prototype.hasOwnProperty.call(currentValue, 'content')) {
-	 currentValue.content = indent(currentValue.content, ++level);
-	}
+	const setIndent = (tree, level = 0) => tree.reduce((previousValue, currentValue, index) => {
+		if (typeof currentValue === 'object' && Object.prototype.hasOwnProperty.call(currentValue, 'content')) {
+			currentValue.content = setIndent(currentValue.content, ++level);
+			--level;
+		}
 
-	// console.log(tree);
-
-	/*if (typeof currentValue === 'object' && !Object.prototype.hasOwnProperty.call(currentValue, 'content')) {
-		currentValue['content'] = getIndent(level);
-	}
-
-	if (level !== 0) {
-		return [...previousValue, getIndent(level), currentValue, getIndent(level)];
-	}*/
-
-	/*if (level !== 0 && index !== 0 ) {
-		return [...previousValue, currentValue, getIndent(level)];
-	}*/
-
-	/*if (level === 0 && (tree.length - 1) !== index) {
-		return [...previousValue, currentValue, getIndent(level)];
-	}*/
-
-	/* if (level !==0 && index === 0) {
-		return previousValue.concat([getIndent(level), currentValue, getIndent(level)]);
-	}
-
-	if (level !== 0 && (tree.length - 1) === index) {
-	   return previousValue.concat([currentValue, getIndent(--level)]);
-	}
-
-	if (level !==0) {
-		return previousValue.concat([currentValue, getIndent(level)]);
-	} */
-
-	/*if (level !== 0) {
-		return previousValue.concat([currentValue, getIndent(level)]);
-	}
-
-	if (level === 0 && (tree.length - 1) !== index) {
-		return previousValue.concat([currentValue, getIndent(level)]);
-	}*/
-
-	if (tree.length === 1) {
-		return [...previousValue, getIndent(++level), currentValue, getIndent(--level)];
-	}
-
-	if ((tree.length - 1) === index && tree.length !== 1) {
-		return [...previousValue, getIndent(--level), currentValue];
-	}
-
-
-
-	return [...previousValue, getIndent(level), currentValue];
-}, []);
-
-function beautify(tree) {
-	return new Promise(resolve => resolve(tree))
-		.then(tree => {
-			// console.log(tree);
-			return clean(tree);
-		})
-		.then(tree => {
-			// console.log(tree);
-			return indent(tree);
-		})
-		.then(tree => {
-			// console.log('---------|end|-----------');
+		if (tree.length === 1 && typeof tree[index] === 'string') {
 			console.log(tree);
-			// console.log('---------|content|-----------');
-			return tree;/*[
-	'<!DOCTYPE html>',
-	'\n',
-	{
-		tag: 'div',
-		content: [
-			'\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class: 'first'
-				},
-				content: [
-					'\n        ',
-					'first',
-					'\n    '
-				]
-			},
-			'\n\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class: 'middle'
-				},
-				content: ['\n    ']
-			},
-			'\n\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class: 'last'
-				},
-				content: [
-					'\n        ',
-					'last',
-					'\n        ',
-					{
-						tag: 'b',
-						content: [
-							'\n            ',
-							'line',
-							'\n        '
-						]
-					},
-					'\n\n        ',
-					{
-						tag: 'a',
-						attrs: {
-							href: '#'
-						},
-						content: [
-							'\n            ',
-							'test',
-							'\n        '
-						]
-					},
-					'\n    '
-				]
-			},
-			'\n'
-		]
-	}
-];*/
-		});
+			return [...previousValue, currentValue];
+		}
+
+		if (level === 0 && (tree.length - 1) === index) {
+			return [...previousValue, currentValue];
+		}
+
+		if (level === 0) {
+			return [...previousValue, currentValue, getIndent(level)];
+		}
+
+		if ((tree.length - 1) === index) {
+			return [...previousValue, getIndent(level), currentValue, getIndent(--level)];
+		}
+
+		return [...previousValue, getIndent(level), currentValue, getIndent(0)];
+	}, []);
+
+	return setIndent(tree);
+};
+
+function beautify(tree, options) {
+	return new Promise(resolve => resolve(tree))
+		.then(tree => clean(tree))
+		.then(tree => indent(tree, options))
+		.then(tree => tree);
 }
 
-export default () => tree => new Promise((resolve, reject) => {
-	if (!Array.isArray(tree)) {
-		reject(new Error(`tree is not Array`));
-	}
+export default (options = {}) => {
+	return tree => new Promise((resolve, reject) => {
+		if (!Array.isArray(tree)) {
+			reject(new Error(`tree is not Array`));
+		}
 
-	if (tree.length === 0) {
-		resolve(tree);
-	}
+		if (tree.length === 0) {
+			resolve(tree);
+		}
 
-	resolve(beautify(tree));
-});
-
-
-/*
-[
-	'<!DOCTYPE html>',
-	'\n'
-	{
-		tag: 'div',
-		content: [
-			'\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class:
-				},
-				content: [
-					'\n        ',
-					'first',
-					'\n    '
-				]
-			},
-			'\n\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class: 'middle'
-				}
-			},
-			'\n\n    ',
-			{
-				tag: 'div',
-				attrs: {
-					class: 'last'
-				},
-				content: [
-					'\n        ' ,
-					'last',
-					{
-						tag: 'b',
-						content: [
-							'\n            ',
-							'line',
-							'\n        '
-						]
-					},
-					'\n    '
-				]
-			},
-			'\n'
-		]
-	}
-]
-*/
-
-
-/*
-var tree = [
-	'<!DOCTYPE html>'
-	{
-		tag: 'div',
-		content: [
-			{
-				tag: 'div',
-				attrs: {
-					class:
-				},
-				content: [
-					'first'
-				]
-			},
-			{
-				tag: 'div',
-				attrs: {
-					class: 'middle'
-				}
-			},
-			{
-				tag: 'div',
-				attrs: {
-					class: 'last'
-				},
-				content: [
-					'last',
-					{
-						tag: 'b',
-						content: [
-							'line'
-						]
-					}
-				]
-			}
-		]
-	},
-	options: {},
-	walk: [Function],
-	match: [Function]
-]
-
-function getIndent(level) {
-		return `\nnew Array(4 * level).join(' ');
-}
-
-var a = function indent(tree, level = 0){
-		return tree.reduce((init, curr, index, arr) => {
-			 if (typeof curr === 'object' && curr.content) {
-
-			 }
-
-			 if (level === 0 && arr.length !== index) {
-					 return [curr, getIndent(level)];
-			 }
-		});
-}(tree);
-
-*/
-
-/*
-<!DOCTYPE html>\n
-<div>\n
-    <div class="first">\n
-        first\n
-    </div>\n\n
-    <div class="middle">\n
-    </div>\n\n
-    <div class="last">\n
-        last\n
-        <b>\n
-            line\n
-        </b>\n
-    </div>\n
-</div>
-
-...
-'\n'
-...
-    '\n    '
-    ...
-        '\n        '
-        ...
-        '\n    '
-    '\n\n    '
-    ...
-        '\n    '
-    '\n\n    '
-    ...
-        '\n        '
-        ...
-        '\n        '
-        ...
-            '\n            '
-            ...
-            '\n        '
-        '\n    '
-    '\n'
-*/
+		resolve(beautify(tree, Object.assign({}, optionsDefault, options)));
+	});
+};
