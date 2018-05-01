80930e1c913b10d5ce37180c8620071ce6ee056f
andreyvolokitin <andreyvlktn@gmail.com>
2018-05-01 16:49:25 +0400

    fix: m regexp flag is not needed

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const optionsDefault = {
 	}
 };
 
-const horizontalWhitespace = /[\t ]+/gm;
-const verticalWhitespace = /[\r\n\v\f]+/gm;
+const horizontalWhitespace = /[\t ]+/g;
+const verticalWhitespace = /[\r\n\v\f]+/g;
 
 const getEdgeWhitespace = (node) => {
 	let leftWhitespace;
@@ -74,7 +74,7 @@ const clean = (tree, options) => {
 				} else {
 					nodeCleaned =
 						`${leftLinebreaks || (previousNodeRightLinebreaks ? '' : leftWhitespace.replace(horizontalWhitespace, ' '))}` +
-						`${nodeTrimmed.replace(horizontalWhitespace, ' ').replace(/([\r\n\v\f]+)[\t ]/gm, '$1')}` +
+						`${nodeTrimmed.replace(horizontalWhitespace, ' ').replace(/([\r\n\v\f]+)[\t ]/g, '$1')}` +
 						`${rightLinebreaks || rightWhitespace.replace(horizontalWhitespace, ' ')}`;
 
 					previousNodeRightLinebreaks = rightLinebreaks;
@@ -143,7 +143,7 @@ const indent = (tree, {rules: {indent, eol, blankLines, useExistingLineBreaks}})
 
 		if (useExistingLineBreaks) {
 			if (typeof node === 'string') {
-				node = node.replace(/([\r\n\v\f]+)/gm, function (match, p1, offset, string) {
+				node = node.replace(/([\r\n\v\f]+)/g, function (match, p1, offset, string) {
 					if ((index === tree.length - 1) && (offset + match.length === string.length)) {
 						--level;
 					}
