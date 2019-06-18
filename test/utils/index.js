e824c60bb88e800e695c75c893240e537736aa2d
Ivan Demidov <Scrum@list.ru>
2019-06-18 12:07:00 +0300

    style: fix indent

--- test/utils/index.js
+++ test/utils/index.js
@@ -4,10 +4,11 @@ import posthtml from 'posthtml';
 export const processing = (html, plugins = [], options = {}) => posthtml(plugins).process(html, options);
 
 export const read = pathFile => new Promise((resolve, reject) => {
-    readFile(pathFile, 'utf8', (err, data) => {
-        if (err) {
-            reject(err);
-        }
-        return resolve(data);
-    });
+  readFile(pathFile, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+    }
+
+    return resolve(data);
+  });
 });
